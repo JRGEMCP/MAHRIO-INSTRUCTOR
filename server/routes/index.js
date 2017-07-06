de016d3699b51bc4a0031df02d06f0fac706d5d0
Jesus Guerrero <rocha.jesus@hotmail.com>
2017-07-05 22:42:39 -0700

    Instructor SPA

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function( server, publicPath ) {
   server.route({
-    path: '/student/assets/{any*}',
+    path: '/instructor/assets/{any*}',
     method: 'GET',
     handler: {
       directory: {
@@ -10,10 +10,9 @@ module.exports = function( server, publicPath ) {
   });
 
   server.route({
-    path: '/student/{any*}',
+    path: '/instructor/{any*}',
     method: 'GET',
     handler: function( req, rep ) {
-      console.log( publicPath + 'index.html' );
       rep.file(publicPath + 'index.html')
     }
   });
